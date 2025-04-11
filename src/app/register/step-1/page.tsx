093cf5ca0cd8151f7667b35e07849f9f519737f2
Billy Thompson <48634582+billythompson@users.noreply.github.com>
2025-04-11 08:55:25 +0100

    add registration step title

--- src/app/register/step-1/page.tsx
+++ src/app/register/step-1/page.tsx
@@ -1,9 +1,13 @@
 import { RegistrationProgressIndicator } from '@/app/components/RegistrationProgressIndicator';
+import { RegistrationStepTitle } from '@/app/components/RegistrationStepTitle';
 
 export default function RegistrationStep1Page() {
   return (
     <main>
       <RegistrationProgressIndicator activeStep='Step 1' />
+      <RegistrationStepTitle>
+        Make a backup of your Post-Quantum seed phrase
+      </RegistrationStepTitle>
     </main>
   );
 }
