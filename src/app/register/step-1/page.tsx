f617b778b0771977e641ae9ed1c61187397920f8
Billy Thompson <48634582+billythompson@users.noreply.github.com>
2025-04-11 13:22:07 +0100

    add warning message

--- src/app/register/step-1/page.tsx
+++ src/app/register/step-1/page.tsx
@@ -10,7 +10,9 @@ import { EyeIcon } from '@/app/icons/EyeIcon';
 import { useCallback, useState } from 'react';
 import { EyeOffIcon } from '@/app/icons/EyeOffIcon';
 import { mockSeedPhrase } from '@/mock-data';
+import { Warning } from '@/app/components/Warning';
 import styles from './styles.module.css';
+import { RegistrationHeader } from '@/app/components/RegistrationHeader';
 
 export default function RegistrationStep1Page() {
   const [isSeedPhraseVisible, setIsSeedPhraseVisible] = useState(false);
@@ -26,8 +28,8 @@ export default function RegistrationStep1Page() {
   );
 
   return (
-    <main className={styles.mainArea}>
-      <div>
+    <main>
+      <RegistrationHeader>
         <RegistrationProgressIndicator activeStep='Step 1' />
         <RegistrationStepTitle>
           Make a backup of your Post-Quantum seed phrase
@@ -35,35 +37,36 @@ export default function RegistrationStep1Page() {
         <p>
           This is the 24-word seed phrase for your new Post-Quantum address.
         </p>
-      </div>
-      <div>
-        <HighlightedBox>
-          <span
-            className={`${styles.seedPhrase} ${isSeedPhraseVisible ? styles.visibleSeedPhrase : ''}`}
-          >
-            {seedPhrase}
-          </span>
-        </HighlightedBox>
-        <Toolbar>
-          <ToolbarButton onClick={copySeedPhrase}>
-            <CopyIcon />
-            Copy
-          </ToolbarButton>
-          <ToolbarButton onClick={toggleSeedPhraseVisibility}>
-            {isSeedPhraseVisible ? (
-              <>
-                <EyeOffIcon />
-                Conceal
-              </>
-            ) : (
-              <>
-                <EyeIcon />
-                Reveal
-              </>
-            )}
-          </ToolbarButton>
-        </Toolbar>
-      </div>
+      </RegistrationHeader>
+      <Warning>
+        Save this somewhere safe and do not share it with anyone
+      </Warning>
+      <HighlightedBox>
+        <span
+          className={`${styles.seedPhrase} ${isSeedPhraseVisible ? styles.visibleSeedPhrase : ''}`}
+        >
+          {seedPhrase}
+        </span>
+      </HighlightedBox>
+      <Toolbar>
+        <ToolbarButton onClick={copySeedPhrase}>
+          <CopyIcon />
+          Copy
+        </ToolbarButton>
+        <ToolbarButton onClick={toggleSeedPhraseVisibility}>
+          {isSeedPhraseVisible ? (
+            <>
+              <EyeOffIcon />
+              Conceal
+            </>
+          ) : (
+            <>
+              <EyeIcon />
+              Reveal
+            </>
+          )}
+        </ToolbarButton>
+      </Toolbar>
     </main>
   );
 }
