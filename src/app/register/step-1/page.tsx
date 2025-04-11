3bd9c64916a3f2f1e063a8f81a76fb6d306d0f9d
Billy Thompson <48634582+billythompson@users.noreply.github.com>
2025-04-11 08:37:03 +0100

    add progress indicator

---
+++ src/app/register/step-1/page.tsx
@@ -0,0 +1,9 @@
+import { RegistrationProgressIndicator } from '@/app/components/RegistrationProgressIndicator';
+
+export default function RegistrationStep1Page() {
+  return (
+    <main>
+      <RegistrationProgressIndicator activeStep='Step 1' />
+    </main>
+  );
+}
