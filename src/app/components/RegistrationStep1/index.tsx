136d5b1a4a07b21f5cfd8d0b91c56eb1c0f9f67b
Billy Thompson <48634582+billythompson@users.noreply.github.com>
2025-04-17 11:42:38 +0100

    add CheckIcon prop

--- src/app/components/RegistrationStep1/index.tsx
+++ src/app/components/RegistrationStep1/index.tsx
@@ -98,7 +98,7 @@ export function RegistrationStep1() {
         <ToolbarButton onClick={copySeedPhrase}>
           {isCopiedIndicatorVisible ? (
             <>
-              <CheckIcon />
+              <CheckIcon stroke='#7fd17f' />
               Copied
             </>
           ) : (
