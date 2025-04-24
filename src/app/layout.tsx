4810a1b439b9a966074d2b504a44e8a76f4c7158
Billy Thompson <48634582+billythompson@users.noreply.github.com>
2025-04-24 09:30:02 +0100

    add NavigationEventManager to clear registration data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import localFont from 'next/font/local';
+import { NavigationEventManager } from '@/app/components/NavigationEventManager';
 import './globals.css';
 
 const geistMono = localFont({
@@ -16,7 +17,9 @@ export default function RootLayout({
       <head>
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       </head>
-      <body className={geistMono.className}>{children}</body>
+      <body className={geistMono.className}>
+        <NavigationEventManager>{children}</NavigationEventManager>
+      </body>
     </html>
   );
 }
