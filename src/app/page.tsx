9e48a827ca9ab18805256e37ed87d7f26675e03c
Billy Thompson <48634582+billythompson@users.noreply.github.com>
2025-05-16 13:57:27 +0100

    update homepage content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,9 +22,14 @@ export default function HomePage() {
   return (
     <main className={styles.homepage}>
       <h1 className={styles.title}>yellowpages.xyz</h1>
-      <p className={styles.message}>
-        Find yourself in the post quantum world - join the yellowpages.
-      </p>
+      <div className={styles.content}>
+        <p>Find yourself in the post quantum world - join the yellowpages.</p>
+        <p>
+          The yellowpages is the only public, anonymous post-quantum proof of
+          Bitcoin ownership.{' '}
+          <Link href='/faqs'>Read our FAQs to learn more</Link>.
+        </p>
+      </div>
       <div className={styles.links}>
         <Link className={styles.primaryLink} href='/register/step-1'>
           Register <ArrowRightIcon />
