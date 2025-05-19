509bbd914317b99fad80f082a3c652cc269b4be0
billythompson <48634582+billythompson@users.noreply.github.com>
2025-05-19 11:39:16 +0100

    Bt/pro 383 copy signing message to clipboard on click (#44)

--- src/app/components/CopyTextToolbarButton/index.tsx
+++ src/app/components/CopyTextToolbarButton/index.tsx
@@ -1,21 +1,28 @@
-import { useCallback, useRef, useState } from 'react';
+import {
+  useCallback,
+  useRef,
+  useState,
+  forwardRef,
+  useImperativeHandle
+} from 'react';
 import { ToolbarButton } from '@/app/components/ToolbarButton';
 import { CheckIcon } from '@/app/icons/CheckIcon';
 import { CopyIcon } from '@/app/icons/CopyIcon';
 
-export function CopyTextToolbarButton({
-  label = 'Copy',
-  onClick
-}: {
-  label?: string;
-  onClick: () => void;
-}) {
+export const CopyTextToolbarButton = forwardRef(function CopyTextToolbarButton(
+  {
+    label = 'Copy',
+    onClick
+  }: {
+    label?: string;
+    onClick: () => void;
+  },
+  ref
+) {
   const [isIndicatorVisible, setIsIndicatorVisible] = useState(false);
   const timeoutRef = useRef<ReturnType<typeof setTimeout>>(null);
 
-  const clickHandler = useCallback(() => {
-    onClick();
-
+  const showSuccessIndicator = useCallback(() => {
     setIsIndicatorVisible(true);
 
     if (timeoutRef.current) {
@@ -26,7 +33,16 @@ export function CopyTextToolbarButton({
       setIsIndicatorVisible(false);
       timeoutRef.current = null;
     }, 1000);
-  }, [onClick]);
+  }, []);
+
+  const clickHandler = useCallback(() => {
+    onClick();
+    showSuccessIndicator();
+  }, [onClick, showSuccessIndicator]);
+
+  useImperativeHandle(ref, () => ({
+    showSuccessIndicator
+  }));
 
   return (
     <ToolbarButton onClick={clickHandler}>
@@ -43,4 +59,4 @@ export function CopyTextToolbarButton({
       )}
     </ToolbarButton>
   );
-}
+});
