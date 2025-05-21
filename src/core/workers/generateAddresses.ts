ab615f662d8ec73c4477df00ea0e7f7cd4b25bee
Billy Thompson <48634582+billythompson@users.noreply.github.com>
2025-05-21 17:07:31 +0100

    use sync event handlers so that error is caught by worker.onerror

--- src/core/workers/generateAddresses.ts
+++ src/core/workers/generateAddresses.ts
@@ -2,7 +2,7 @@ import { generateAddresses, Mnemonic24 } from '../cryptography';
 
 addEventListener(
   'message',
-  async (event: MessageEvent<{ mnemonic24: Mnemonic24 }>) => {
+  (event: MessageEvent<{ mnemonic24: Mnemonic24 }>) => {
     const input = event.data;
     const result = generateAddresses(input.mnemonic24);
     postMessage(result);
--- src/core/workers/generateSignedMessages.ts
+++ src/core/workers/generateSignedMessages.ts
@@ -2,9 +2,7 @@ import { generateSignedMessages, Mnemonic24 } from '../cryptography';
 
 addEventListener(
   'message',
-  async (
-    event: MessageEvent<{ mnemonic24: Mnemonic24; bitcoinAddress: string }>
-  ) => {
+  (event: MessageEvent<{ mnemonic24: Mnemonic24; bitcoinAddress: string }>) => {
     const input = event.data;
     const result = generateSignedMessages(
       input.mnemonic24,
