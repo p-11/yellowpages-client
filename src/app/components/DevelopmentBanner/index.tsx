c4b782fcc8b64aeac9da8f3e0236e5e24274f23c
Billy Thompson <48634582+billythompson@users.noreply.github.com>
2025-06-04 13:48:38 +0100

    add button to development banner to skip step 2

--- src/app/components/DevelopmentBanner/index.tsx
+++ src/app/components/DevelopmentBanner/index.tsx
@@ -1,6 +1,12 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 import styles from './styles.module.css';
 
 export function DevelopmentBanner() {
+  const pathname = usePathname();
+
   return (
     <div className={styles.banner}>
       <div className={styles.bannerContent}>
@@ -9,6 +15,13 @@ export function DevelopmentBanner() {
           environment does not register your Bitcoin address on the yellowpages.
           Do not register a Bitcoin address with mainnet funds.
         </span>
+        {pathname === '/register/step-2' && (
+          <div>
+            <Link href='/register/step-3' className={styles.skipStepButton}>
+              Skip this step
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
