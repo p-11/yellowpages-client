82a079cadf751650b9fb7acd8a2d7e9c018bdd3a
Billy Thompson <48634582+billythompson@users.noreply.github.com>
2025-06-06 10:05:18 +0100

    retain trace on first test failure

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,8 +28,8 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://localhost:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry'
+    /* Collect trace when a test fails. See https://playwright.dev/docs/trace-viewer */
+    trace: 'retain-on-first-failure'
   },
 
   globalSetup: require.resolve('./playwright-global.setup'),
