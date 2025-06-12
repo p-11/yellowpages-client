5ed448ae30ab986cb572aa302266689d05c82f49
davidnugent2425 <david.nugent2425@gmail.com>
2025-06-12 10:32:33 +0100

    fix babel stuff

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: [],
-  plugins: [],
-  env: {
-    test: {
-      plugins: ['babel-plugin-transform-import-meta']
-    }
-  }
-}; 
--- jest.config.js
+++ jest.config.js
@@ -1,35 +1,28 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  // Use ts-jest to transform both .ts/.tsx and .js/.jsx
-  preset: 'ts-jest/presets/js-with-ts',
+/** @type {import('jest').Config} */
+const config = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
-
-  // Enable experimental fetch
-  setupFiles: ['<rootDir>/jest.setup.js'],
-
-  // Set NODE_ENV to test to ensure Babel plugins are applied
-  setupFilesAfterEnv: [],
-  globals: {
-    'ts-jest': {
-      babelConfig: true
-    }
-  },
-
-  // Transform everything under src with ts-jest, and the WASM package with Babel
   transform: {
-    '^.+\\.[jt]sx?$': 'ts-jest',
-    '^.+/node_modules/@evervault/wasm-attestation-bindings/index\\.js$': 'babel-jest'
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: 'tsconfig.json',
+      babelConfig: './src/__tests__/babel.config.js'
+    }],
+    '^.+\\.js$': ['babel-jest', {
+      configFile: './src/__tests__/babel.config.js'
+    }]
   },
   transformIgnorePatterns: [
-    // Ignore all node_modules except specific ESM packages
-    '/node_modules/(?!(bitcoin-address-validation|base58-js|@evervault\\/wasm-attestation-bindings)/)'
+    // Transform ESM modules that we need
+    '/node_modules/(?!(bitcoin-address-validation|base58-js|@noble|@evervault|@scure)/)'
   ],
-
-  // If you use path-aliases (e.g. @/…), map them:
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
-
-  // ignore e2e test files
-  testPathIgnorePatterns: ['\\.e2e\\.']
+  // Exclude e2e tests and config files
+  testPathIgnorePatterns: [
+    '\\.e2e\\.test\\.[jt]sx?$',
+    'babel\\.config\\.js$'
+  ]
 };
+
+module.exports = config;
