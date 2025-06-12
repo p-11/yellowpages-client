2fb5dd8e56262f8ee98c78396a170d0649949788
Billy Thompson <48634582+billythompson@users.noreply.github.com>
2025-06-12 18:26:22 +0100

    add slhdsaSha2S128 to og-image route

--- src/app/share/page.tsx
+++ src/app/share/page.tsx
@@ -2,18 +2,24 @@ import type { Metadata } from 'next';
 import { SharePageRedirection } from '@/app/components/SharePageRedirection';
 
 type Props = {
-  searchParams: Promise<{ btc: string; mldsa44: string }>;
+  searchParams: Promise<{
+    btc: string;
+    mldsa44: string;
+    slhdsaSha2S128: string;
+  }>;
 };
 
 export async function generateMetadata({
   searchParams
 }: Props): Promise<Metadata> {
-  const { btc, mldsa44 } = await searchParams;
+  const { btc, mldsa44, slhdsaSha2S128 } = await searchParams;
 
   return {
     title: 'yellowpages',
     openGraph: {
-      images: [`/og-image?btc=${btc}&mldsa44=${mldsa44}`]
+      images: [
+        `/og-image?btc=${btc}&mldsa44=${mldsa44}&slhdsaSha2S128=${slhdsaSha2S128}`
+      ]
     }
   };
 }
