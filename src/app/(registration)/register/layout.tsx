1c4943e6545656e4f5d8c35d4339a4fcc3c49750
Billy Thompson <48634582+billythompson@users.noreply.github.com>
2025-06-23 13:00:41 +0100

    add registrations paused notice

--- src/app/(registration)/register/layout.tsx
+++ src/app/(registration)/register/layout.tsx
@@ -1,3 +1,4 @@
+import { RegistrationsPausedNotice } from '@/app/components/RegistrationsPausedNotice';
 import { RegistrationSessionProvider } from '../../providers/RegistrationSessionProvider';
 
 export default function RegisterLayout({
@@ -5,5 +6,9 @@ export default function RegisterLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  if (process.env.BOT_PROTECTION_ENABLED) {
+    return <RegistrationsPausedNotice />;
+  }
+
   return <RegistrationSessionProvider>{children}</RegistrationSessionProvider>;
 }
---
+++ src/app/components/RegistrationsPausedNotice/index.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useCallback } from 'react';
+import { useRouter } from 'next/navigation';
+import { Button } from '@/app/components/Button';
+import styles from './styles.module.css';
+
+export function RegistrationsPausedNotice() {
+  const router = useRouter();
+
+  const navigateToHomepage = useCallback(() => {
+    router.push('/');
+  }, [router]);
+
+  return (
+    <main>
+      <h1 className={styles.title}>Registraion is temporarily paused</h1>
+      <div className={styles.content}>
+        <p>Please check back later and try again.</p>
+        <div className={styles.footer}>
+          <Button variant='primary' onClick={navigateToHomepage}>
+            Directory home
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
+}
